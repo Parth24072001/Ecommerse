ba4609b39f2132cbf63287fa042bd6a114d16996
parth <poshiyaparth2407@gmail.com>
2022-09-23 17:58:47 +0530

    Add Router

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
-
-return <Wrapper></Wrapper>;
+const SingleProduct=()=>{
+return <Wrapper>
+  <h1>single product</h1>
+</Wrapper>;
+}
 
 const Wrapper = styled.section`
   .container {
